1ac3aa366a0b6c8fbb608572f0a644110333c93a
Eko Eryanto <ekoeryanto@gmail.com>
2018-08-12 21:44:17 +0700

    chore: defer on netlify-identity

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Netlify CMS + VuePress',
   description: 'Netlify + VuePress',
   head: [
-    ['script', { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    ['script', { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js', defer: true }]
   ],
   themeConfig: {
     docsDir: 'docs',
